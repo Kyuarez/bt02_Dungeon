2bad36b404cc49462ea61b11eb89fbce18569ea1
Kyuarez <97830742+Kyuarez@users.noreply.github.com>
2025-02-21 09:53:56 +0900

    02.21 cummit
    #tile setting
    #tcp server structure setting

---
+++ TcpServer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TcpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Dedicated Server";
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TcpServer/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+
+        public void Start()
+        {
+
+        }
+
+
+    }
+}
